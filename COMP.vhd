11983d96dd041c4057a04ebea122f08fdeed8a0b
Arthur W <apwinck@gmail.com>
2019-11-19 18:24:55 -0300

    Create COMP.vhd

---
+++ COMP.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.Std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+entity COMP is
+	port(	OUT_FPGA, OUT_User: in std_logic_vector(63 downto 0);
+			COMP_end: in std_logic
+	);
+end COMP;
+
+architecture comp of COMP is
+begin
+	COMP_end <= '1' when OUT_FPGA = OUT_user else '0';
+end comp;
